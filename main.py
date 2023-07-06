1c0f7914b68844f05d6759bb9c3fb864b293890d
hothaifa zoubi <hothaifazoubi@gmail.com>
2023-07-06 20:03:02 +0300

    qa 34

---
+++ main.py
@@ -0,0 +1,137 @@
+# this is a comment section
+
+# print('hello world!')  # printing hello code
+#
+# print('hothaifa zoubi')
+#
+# print(5.5 - 1.8)
+
+# vars - משתנים
+# string - word letter number -> ''  "" str
+# name = 'hodi'
+# print(name)
+# name = 'amir'
+# print(name)
+
+# int
+# age = 5 + 122
+# print(age + 100 )
+# print(age)
+
+# float
+# avg = 94.7
+# print(avg)
+
+# bool
+
+# single = False
+#
+# print(single)
+#
+
+# x = 15
+# y = 4
+
+# print(x + y)  # 19
+# print(x - y)  # 11
+# print(x * y)  # 60
+# print(x ** y)  # x^y
+# print(x / y)  # 3.75 float
+# print(x // y)  # 3
+# print(x % y) # 3
+
+# tahini = 5
+# friends = 2
+#
+# print( tahini // friends )
+# print( tahini % friends )
+
+
+# odd   3  49  59  55  11457
+# even  2  0     190844  48 16
+# a = 1
+# print(a%2)
+
+
+#------------------------------------------------------------------
+
+
+# # naming the variables
+# name2 = 'hodi'
+# last_name = 'zoubi'
+#
+# print(name2)
+# print(last_name)
+
+# amir_salary = 14000
+# tal_salary = 150000.19
+# h='False'
+#
+# print(type(amir_salary))
+# print(type(tal_salary))
+# print(type(h))
+
+# casting
+# x = int(3)    # 3
+# y = float(3)  # 3.0
+# z = str(3)    # '3'
+#
+# print(type(x))
+# print(type(y))
+# print(type(z))
+#
+# h = '-3.49'  # cast the number to int
+# h = float(h)
+# print(h)
+
+
+
+#----------------------
+# sen = 'this is my FIRST SEN in python'
+#
+# print(sen.upper())
+# print(sen.lower())
+# print(sen.title())
+# print(len(sen))
+# print(sen.lower().count('is'))
+
+
+# name = input('whats your name ?')
+#
+# print(name.upper())
+
+
+# write a python code to receive
+# the user pet name and print the length of the name
+
+# pet_name = input('please enter your pet name : ')
+# print(len(pet_name))
+
+# phrase = 'live what you love'
+
+# print(phrase[-2]) # v
+# print(phrase[-1]) # e
+# print(phrase[1]) # i
+# print(phrase[0]) # l
+# print(phrase[0:4]) # live
+# print(phrase[-4:]) # love
+# print(phrase[:7]) # love
+# print(phrase[-10:10]) # love
+
+# print(phrase[10::-1])
+
+# phrase[start:end:jump]
+
+
+
+# קלטו מהמשתש מספר הטלפון שלו ותדפסיו את המספר בלי הקידומת
+#קידומת של מספר טלפון היא 3 ספרות הראשונות
+
+phone_number = input('phone number please ?')
+
+print(phone_number[:3])
+print(phone_number[3:])
+
+
+
+
