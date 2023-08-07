3c406e9f3a141cd92285b6b813bfa935212a9422
hothaifa zoubi <hothaifazoubi@gmail.com>
2023-08-07 19:58:06 +0300

    message

---
+++ 7-8/exp.py
@@ -0,0 +1,54 @@
+# try:
+#     x = int(input('please enter integer'))
+#     print(x)
+# except:
+#     print('something went wrong')
+# finally:
+#     print('hello')
+#
+# try:
+#     file = open('file1.txt', 'r')
+#     file.write('hello')
+# except:
+#     print('something went wrong')
+# finally:
+#     file.close()
+
+
+# arr = [1, 2, 3]
+# try:
+#     print(arr[14])
+# except:
+#     print(arr[0])
+
+# try:
+#     row1
+#     row1
+#     row1
+#     row1
+#     row1
+#     row1
+# except:
+
+
+# try:
+#     num1 = int(input('please enter a number '))
+#     num2 = int(input('please enter a number '))
+#     if num2 > num1:
+#         raise Exception('you cannot use those numbers')
+#     print(num1/num2)
+# except ZeroDivisionError:
+#     print('you cannot enter 0 as the base for division')
+# except ArithmeticError:
+#     print('hiii')
+# except ValueError:
+#     print('numbers only you idiot')
+# except Exception:
+#     print('lower numbers next time')
+
+# name = input('please enter your name :')
+#
+# if len(name) > 8:
+#     raise Exception('enter another name ')
+
+
---
+++ 7-8/functions_homework_sol.py
@@ -0,0 +1,106 @@
+# function - requests pytest
+# break
+# final project
+# pytest
+import datetime
+import math
+import time
+import random
+
+import requests
+
+
+# def function_name(*person):
+#     print(person)
+#
+#
+# function_name('hodi','zoubi',15,1700.2)
+
+# 3
+# def foo(n,h=9):
+#     print(f'{str(datetime.datetime.now())} \n' * n)
+#     print(end=)
+#
+#
+# rows = 5
+# foo(rows,5)
+#
+# time.sleep(3)
+#
+# foo(2)
+
+# 4
+# 5
+# def foo2(name='yonatan', age=88):
+#     print(name, age)
+#
+#
+# foo2(age=99)
+# print('hello', end='\t')
+
+# 5
+# def foo3(r):
+#     area = math.pi * r ** 2
+#     return area
+#
+#
+# # 6
+# def foo4():
+#     pass
+#
+# x = random.randrange(1,100,2)
+# print(x)
+
+# def calc_hekef(r):
+#     '''
+#     :param r: the radius given by the user
+#     :return: the hekef of the circle
+#     '''
+#     hekef = 2 * math.pi * r
+#     return hekef
+#
+#
+# print(calc_hekef(25))
+
+# 9
+
+# def add(x=0, y=0):
+#     return x + y
+#
+#
+# def sub(x=0, y=0):
+#     return x - y
+#
+#
+# def mul(x=0, y=0):
+#     return x * y
+#
+#
+# def div(x=0, y=1):
+#     return x / y
+#
+#
+# number1 = float(input('please enter a number : '))
+# number2 = float(input('please enter a number : '))
+#
+# print(div(number1, number2))
+# print(add(number1, number2))
+# print(mul(number1, number2))
+# print(sub(number1, number2))
+
+# 10
+
+# def get_in_range(min, max):
+#     while True:
+#         value = int(input('please enter a number :: '))
+#         if min <= value <= max:
+#             return value
+#
+#
+# print('and the number is : ' + str(get_in_range(50, 100)))
+
+# def my_min(a, b, c):
+#     return a if c > a < b else b if b < c else c
+#
+#
+# print(my_min(5, 7, -99))
